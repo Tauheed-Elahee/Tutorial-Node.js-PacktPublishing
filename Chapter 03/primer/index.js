76458d4631f64ae2ed383e6ae26c4d0634c7e595
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-02-02 08:13:19 -0500

    Add exercising showing JavaScript is weakly and dynamically typed

--- Chapter 03/primer/index.js
+++ Chapter 03/primer/index.js
@@ -1,1 +1,9 @@
-console.log("Hello, World");
+function sum (first, second) {
+    return first + second;
+}
+
+let result = sum(10, 10);
+console.log(`Result value: ${result}, Result type: ${typeof result}`);
+
+result = sum(10, "10");
+console.log(`Result value: ${result}, Result type: ${typeof result}`);
