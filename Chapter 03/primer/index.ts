b5e5af6dc14119cfae32db0b94aab54184f272a9
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-02-05 20:55:30 -0500

    Show how to use if else and case statements in TypeScript and JavaScript

--- Chapter 03/primer/index.ts
+++ Chapter 03/primer/index.ts
@@ -34,3 +34,23 @@ let hexValue = 0xFFFF;
 
 let person1 = "Alice";
 let person2: string | undefined = "Bob";
+
+let firstName = "Adam";
+if (firstName == "Adam") {
+    console.log("firstName is Adam");
+} else if (firstName == "Jacqui") {
+    console.log("firstName is Jacqui");
+} else {
+    console.log("firstName is neither Adam or Jacqui");
+}
+switch(firstName) {
+    case "Adam":
+        console.log("firstName is Adam");
+        break;
+    case "Jacqui":
+        console.log("firstName is Jacqui");
+        break;
+    default:
+        console.log("firstName is neither Adam or Jacqui");
+        break;
+}
