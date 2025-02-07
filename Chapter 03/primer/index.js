cb37941c57e9decda49bb940f7f69b8b173edb31
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-02-06 22:56:43 -0500

    Replace null coalescing operator with nullish coalescing operator to get correct result for coalesced3 because val3 = 0 it did not work with just the null coalescing operator.

--- Chapter 03/primer/index.js
+++ Chapter 03/primer/index.js
@@ -64,9 +64,9 @@ else {
 let val1;
 let val2 = "London";
 let val3 = 0;
-let coalesced1 = val1 || "fallback value";
-let coalesced2 = val2 || "fallback value";
-let coalesced3 = val3 || 100;
+let coalesced1 = val1 ?? "fallback value";
+let coalesced2 = val2 ?? "fallback value";
+let coalesced3 = val3 ?? 100;
 console.log(`Result 1: ${coalesced1}`);
 console.log(`Result 2: ${coalesced2}`);
 console.log(`Result 3: ${coalesced3}`);
