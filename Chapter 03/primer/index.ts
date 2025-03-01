69f87054df99c5b0af886056210df5aaec2e8bc7
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-03-01 18:23:53 -0500

    Create opjects using literal syntax.

--- Chapter 03/primer/index.ts
+++ Chapter 03/primer/index.ts
@@ -136,3 +136,16 @@ console.log("---");
 let otherArray = [...myArray, 200, "Bob", false];
 otherArray.forEach((value, index) =>
     console.log("Index " + index + ": " + value));
+
+
+let hat = {
+    name: "Hat",
+    price: 100
+};
+let boots = {
+    name: "Boots",
+    price: 100
+};
+
+console.log(`Name: ${hat.name}, Price: ${hat.price}`);
+console.log(`Name: ${boots.name}, Price: ${boots.price}`);
