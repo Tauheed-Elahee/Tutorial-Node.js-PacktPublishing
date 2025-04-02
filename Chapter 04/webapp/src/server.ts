d99a6fa9d230086fd981c1430902892381f7f49a
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-04-02 18:08:35 -0400

    Change Node.js server creation and callback function association from using Node.js features that hide away details to associating callback functions to events directly.

--- Chapter 04/webapp/src/server.ts
+++ Chapter 04/webapp/src/server.ts
@@ -1,7 +1,17 @@
 import { createServer } from "http";
 import { handler } from "./handler";
+
+// Use Node.js API
+// const port = 5000;
+// const server = createServer(handler);
+// server.listen(port, function () {
+//   console.log(`Server listening on port ${port}`);
+// });
+
 const port = 5000;
-const server = createServer(handler);
-server.listen(port, function () {
-  console.log(`Server listening on port ${port}`);
-});
+const server = createServer();
+server.on("request", handler);
+server.listen(port);
+server.on("listening", () => {
+  console.log(`(Event) Server listening on port ${port}`);
+})
