94c40391223108e37b8d500c711f4d753867fc5b
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-04-07 21:58:09 -0400

    Change hanlder from using callbacks to promises.

--- Chapter 04/webapp/src/handler.ts
+++ Chapter 04/webapp/src/handler.ts
@@ -1,14 +1,26 @@
 import { IncomingMessage, ServerResponse } from "http";
-import { readFile } from "fs";
+// import { readFile } from "fs";
+import { readFile } from "fs/promises";
 export const handler = (req: IncomingMessage, res: ServerResponse) => {
-  readFile("data.json", (err: Error | null, data: Buffer) => {
-    if (err == null) {
-      res.end(data, () => console.log("File sent"));
-    } else {
-      console.log(`Error: ${err.message}`);
-      res.statusCode = 500;
-      res.end();
-    }
+  // Use callbacks from the "fs" module
+  // readFile("data.json", (err: Error | null, data: Buffer) => {
+  //   if (err == null) {
+  //     res.end(data, () => console.log("File sent"));
+  //   } else {
+  //     console.log(`Error: ${err.message}`);
+  //     res.statusCode = 500;
+  //     res.end();
+  //   }
+  // });
+
+  // Use Promises
+  const p: Promise<Buffer> = readFile("data.json");
+  p.then((data: Buffer) => res.end(data, () => console.log("File sent")));
+  p.catch((err: Error) => {
+    console.log(`Error: {err.message}`);
+    res.statusCode = 500;
+    res.end();
   });
+
   // res.end("Hello World");
 };
