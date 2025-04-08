28ffc59de2aba60cbd1dc9b68f524e14236e13a7
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-04-07 22:02:04 -0400

    Chain then and catch methods.

--- Chapter 04/webapp/src/handler.ts
+++ Chapter 04/webapp/src/handler.ts
@@ -14,13 +14,13 @@ export const handler = (req: IncomingMessage, res: ServerResponse) => {
   // });
 
   // Use Promises
-  const p: Promise<Buffer> = readFile("data.json");
-  p.then((data: Buffer) => res.end(data, () => console.log("File sent")));
-  p.catch((err: Error) => {
-    console.log(`Error: {err.message}`);
-    res.statusCode = 500;
-    res.end();
-  });
+  const p: Promise<Buffer> = readFile("data.json")
+    .then((data: Buffer) => res.end(data, () => console.log("File sent")))
+    .catch((err: Error) => {
+      console.log(`Error: {err.message}`);
+      res.statusCode = 500;
+      res.end();
+    });
 
   // res.end("Hello World");
 };
