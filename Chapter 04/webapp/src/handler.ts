932487262d21ad41bfa97b57c7ddf9b373f21d3f
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-04-11 13:39:23 -0400

    Add a blocking operation written in JavaScript to hang the main thread to show that subsequent HTTP requests are blocked by the blocking operation.

--- Chapter 04/webapp/src/handler.ts
+++ Chapter 04/webapp/src/handler.ts
@@ -1,7 +1,7 @@
 import { IncomingMessage, ServerResponse } from "http";
 // import { readFile } from "fs";
-import { readFile } from "fs/promises";
-import { endPromise } from "./promises";
+// import { readFile } from "fs/promises";
+import { endPromise, writePromise } from "./promises";
 // export const handler = (req: IncomingMessage, res: ServerResponse) => {
 //   // Use callbacks from the "fs" module
 //   // readFile("data.json", (err: Error | null, data: Buffer) => {
@@ -26,16 +26,34 @@ import { endPromise } from "./promises";
 // 
 //   // res.end("Hello World");
 // };
+
+// Use promises to read files
+//export const handler = async (req: IncomingMessage, res: ServerResponse) => {
+//  try{
+//    const data: Buffer = await readFile("data.json");
+//     res.end is a callback function
+//     res.end(data, () => console.log("File sent"));
+//    await endPromise.bind(res)(data);
+//    console.log("File sent");
+//  } catch (err: any) {
+//    console.log(`Error: ${err?.message ?? err}`);
+//    res.statusCode = 500;
+//    res.end();
+//  }
+//};
+
+const total = 2_000_000_000;
+const iterations = 5;
+let shared_counter = 0;
 export const handler = async (req: IncomingMessage, res: ServerResponse) => {
-  try{
-    const data: Buffer = await readFile("data.json");
-    // res.end is a callback function
-    // res.end(data, () => console.log("File sent"));
-    await endPromise.bind(res)(data);
-    console.log("File sent");
-  } catch (err: any) {
-    console.log(`Error: ${err?.message ?? err}`);
-    res.statusCode = 500;
-    res.end();
+  const request = shared_counter++;
+  for (let iter = 0; iter < iterations; iter++) {
+    for (let count = 0; count < total; count++) {
+      count++;
+    }
+    const msg = `Request: ${request}, Iterations: ${(iter)}`;
+    console.log(msg);
+    await writePromise.bind(res)(msg + "\n");
   }
-};
+  await endPromise.bind(res)("Done");
+}
