48cc211db5985d5996caa7def571c226f9a03a8a
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-04-11 12:37:56 -0400

    Include additional logic to filter out requests for 'favicon.ico' and respond to those requests with a 404 status code.

--- Chapter 04/webapp/src/server.ts
+++ Chapter 04/webapp/src/server.ts
@@ -10,7 +10,14 @@ import { handler } from "./handler";
 
 const port = 5000;
 const server = createServer();
-server.on("request", handler);
+server.on("request", (req, res) => {
+  if (req.url?.endsWith("favicon.ico")) {
+    res.statusCode = 404;
+    res.end()
+  } else {
+    handler(req, res)
+  }
+});
 server.listen(port);
 server.on("listening", () => {
   console.log(`(Event) Server listening on port ${port}`);
