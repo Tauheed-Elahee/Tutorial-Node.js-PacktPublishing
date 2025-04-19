014a98d9ed726248e1b7fb905add042f7a38f8bc
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-04-19 09:50:31 -0400

    Use header property of IncomingMessage class to retrieve header information.

--- Chapter 05/webapp/src/handler.ts
+++ Chapter 05/webapp/src/handler.ts
@@ -1,4 +1,8 @@
 import { IncomingMessage, ServerResponse } from "http";
 export const handler = async  (req: IncomingMessage, res: ServerResponse) => {
+  console.log(`---HTTP Method: ${req.method}, URL: ${req.url}`);
+  console.log(`host: ${req.headers.host}`);
+  console.log(`accept: ${req.headers.accept}`);
+  console.log(`user-agent: ${req.headers["user-agent"]}`);
   res.end("Hello, World");
 };
