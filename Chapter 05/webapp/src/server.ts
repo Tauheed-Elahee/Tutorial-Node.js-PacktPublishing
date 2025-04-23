2385cec96bb01debb2ea9f916e66c80a0af1342d
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-04-22 22:02:21 -0400

    Add HTTPS server to handle HTTPS requests.

--- Chapter 05/webapp/src/server.ts
+++ Chapter 05/webapp/src/server.ts
@@ -1,11 +1,21 @@
 import { createServer } from "http";
+import { createServer as createHttpsServer } from "https";
+import { readFileSync } from "fs";
 import { handler } from "./handler";
 
 const port = 5000;
+const https_port = 5500;
 const server = createServer(handler);
 // server.on("request", handler);
 server.listen(port,
   () => console.log(`(Event) Server listening on port ${port}`));
+const httpsConfig = {
+  key: readFileSync("key.pem"),
+  cert: readFileSync("cert.pem")
+};
+const httpsServer = createHttpsServer(httpsConfig, handler);
+httpsServer.listen(https_port,
+  () => console.log(`HTTPS Server listening on port ${https_port}`));
 // server.on("listening", () => {
 //   console.log(`(Event) Server listening on port ${port}`);
 // });
