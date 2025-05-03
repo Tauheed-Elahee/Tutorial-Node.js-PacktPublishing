ef24e5211349113b748a0a5242a730f5acfd9019
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-05-03 19:44:55 -0400

    Pipe data from the readable stream to a writable strem. In this case it is a stream from the client then piped to a stream to the client.

--- Chapter 06/webapp/src/readHandler.ts
+++ Chapter 06/webapp/src/readHandler.ts
@@ -13,11 +13,17 @@ import { IncomingMessage, ServerResponse } from "http";
 //  }
 
 //  Use an iterator to read data from the stream
+//  export const readHandler = async (req: IncomingMessage, resp: ServerResponse) => {
+//      req.setEncoding("utf-8");
+//      for await (const data of req) {
+//          console.log(data);
+//      }
+//      console.log("End: all data read");
+//      resp.end();
+//  }
+
+//  Pipe data from the readable stream to a writable strem
+//  In this case it is a stream from the client then piped to a stream to the client
 export const readHandler = async (req: IncomingMessage, resp: ServerResponse) => {
-    req.setEncoding("utf-8");
-    for await (const data of req) {
-        console.log(data);
-    }
-    console.log("End: all data read");
-    resp.end();
+    req.pipe(resp);
 }
