134916da0be7c8ae92d31825008451d0b9a6b06d
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-05-03 19:40:22 -0400

    Use an iterator to read data from the stream.

--- Chapter 06/webapp/src/readHandler.ts
+++ Chapter 06/webapp/src/readHandler.ts
@@ -1,11 +1,23 @@
 import { IncomingMessage, ServerResponse } from "http";
-export const readHandler = (req: IncomingMessage, resp: ServerResponse) => {
+
+//  Attach callback functions to events.
+//  export const readHandler = (req: IncomingMessage, resp: ServerResponse) => {
+//    req.setEncoding("utf-8");
+//    req.on("data", (data: string) =>{
+//        console.log(data);
+//    });
+//    req.on("end", () => {
+//        console.log("End: all data read");
+//        resp.end();
+//    })
+//  }
+
+//  Use an iterator to read data from the stream
+export const readHandler = async (req: IncomingMessage, resp: ServerResponse) => {
     req.setEncoding("utf-8");
-    req.on("data", (data: string) =>{
+    for await (const data of req) {
         console.log(data);
-    });
-    req.on("end", () => {
-        console.log("End: all data read");
-        resp.end();
-    })
+    }
+    console.log("End: all data read");
+    resp.end();
 }
