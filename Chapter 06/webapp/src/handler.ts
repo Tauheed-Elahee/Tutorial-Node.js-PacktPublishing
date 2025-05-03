993abf582689e2ad8b7f4a58e2ef0e404dc03c55
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-05-03 11:29:15 -0400

    Writing a lot of data to the buffer. Writing until the buffer is full, then waiting for the buffer to be drained and then resuming writing to the buffer.

--- Chapter 06/webapp/src/handler.ts
+++ Chapter 06/webapp/src/handler.ts
@@ -2,9 +2,25 @@ import { IncomingMessage, ServerResponse } from "http";
 
 export const basicHandler = (req: IncomingMessage, resp: ServerResponse) => {
   resp.setHeader("Content-Type", "text/plain");
-  for (let i = 0; i < 10; i++) {
-    resp.write(`Message: ${i}\n`);
-  }
+  let total     : number  = 10_000;
+  let i         : number  = 0;
+  let canWrite  : boolean = true;
+  console.log("Start writing data.");
+  const writeData = () => {
+    do {
+      canWrite = resp.write(`Message: ${i++}\n`);
+    } while ((i < total) && canWrite);
 
-  resp.end("End");
+    console.log("Buffer at capacity.");
+    
+    if (i < total) {
+      resp.once("drain", () => {
+        console.log("Buffer has been drained.");
+        writeData();
+      })
+    } else {
+      resp.end();
+    }
+  }
+  writeData();
 }
