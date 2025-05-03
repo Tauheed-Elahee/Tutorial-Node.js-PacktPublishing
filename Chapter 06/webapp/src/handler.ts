d7fedfdfae22718d999a8360d9ecb069d09b12be
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-05-03 12:13:42 -0400

    Setup html page to interact with Node.js server through HTTP stream. Routing done with Express js.

--- Chapter 06/webapp/src/handler.ts
+++ Chapter 06/webapp/src/handler.ts
@@ -1,26 +1,7 @@
 import { IncomingMessage, ServerResponse } from "http";
+import { readFileSync } from "fs";
 
 export const basicHandler = (req: IncomingMessage, resp: ServerResponse) => {
-  resp.setHeader("Content-Type", "text/plain");
-  let total     : number  = 10_000;
-  let i         : number  = 0;
-  let canWrite  : boolean = true;
-  console.log("Start writing data.");
-  const writeData = () => {
-    do {
-      canWrite = resp.write(`Message: ${i++}\n`);
-    } while ((i < total) && canWrite);
-
-    console.log("Buffer at capacity.");
-    
-    if (i < total) {
-      resp.once("drain", () => {
-        console.log("Buffer has been drained.");
-        writeData();
-      })
-    } else {
-      resp.end();
-    }
-  }
-  writeData();
-}
+  resp.write(readFileSync("static/index.html"));
+  resp.end();
+};
--- Chapter 06/webapp/src/server.ts
+++ Chapter 06/webapp/src/server.ts
@@ -1,10 +1,12 @@
 import { createServer } from "http";
 import express, { Express } from "express";
 import { basicHandler } from "./handler";
+import { readHandler } from "./readHandler";
 
 const port = 5000;
 const expressApp: Express = express();
 expressApp.get("*", basicHandler);
+expressApp.post("/read", readHandler);
 const server = createServer(expressApp);
 server.listen(port,
   () => console.log(`HTTP Server listening on port: ${port}`));
