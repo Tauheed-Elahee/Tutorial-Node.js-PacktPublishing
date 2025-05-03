dab53de7c3e3f896a26751c5cc45847261817ae1
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-05-03 11:02:37 -0400

    Write messages to the HTTP stream using the write method

--- Chapter 06/webapp/src/handler.ts
+++ Chapter 06/webapp/src/handler.ts
@@ -1,5 +1,9 @@
 import { IncomingMessage, ServerResponse } from "http";
 
 export const basicHandler = (req: IncomingMessage, resp: ServerResponse) => {
-  resp.end("Hello, World");
+  for (let i = 0; i < 10; i++) {
+    resp.write(`Message: ${i}\n`);
+  }
+
+  resp.end("End");
 }
