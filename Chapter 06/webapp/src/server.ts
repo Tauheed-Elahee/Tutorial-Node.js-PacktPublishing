41fa187d4dd268c955baa62f4be75e485256d1a5
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-05-04 12:28:36 -0400

    Refactor JavaScript into seperate client.js file and use Express middleware component to route requests for static files.

--- Chapter 06/webapp/src/server.ts
+++ Chapter 06/webapp/src/server.ts
@@ -1,12 +1,19 @@
 import { createServer } from "http";
 import express, { Express } from "express";
-import { basicHandler } from "./handler";
+// import { basicHandler } from "./handler";
 import { readHandler } from "./readHandler";
 
 const port = 5000;
 const expressApp: Express = express();
-expressApp.get("*", basicHandler);
+//  Use basic handler
+//  expressApp.get("/favicon.ico", (req, resp) => {
+//    resp.statusCode = 404;
+//    resp.end();
+//  })
+//  expressApp.get("*", basicHandler);
 expressApp.post("/read", readHandler);
+// Use the Express App static middleware with the .use method to route requests to serve static files from the static folder
+expressApp.use(express.static("static"));
 const server = createServer(expressApp);
 server.listen(port,
   () => console.log(`HTTP Server listening on port: ${port}`));
