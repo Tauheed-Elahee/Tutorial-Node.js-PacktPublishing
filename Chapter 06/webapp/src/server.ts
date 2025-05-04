f0cf82073bbe89c8e153db076dfe0b27ff4a6c3c
Tauheed Elahee <online-accounts@tauheed-elahee.com>
2025-05-04 13:21:57 -0400

    Use Express file response enhancements to make express routes to show and download static files.

--- Chapter 06/webapp/src/server.ts
+++ Chapter 06/webapp/src/server.ts
@@ -1,5 +1,5 @@
 import { createServer } from "http";
-import express, { Express } from "express";
+import express, { Express, Request, Response } from "express";
 // import { basicHandler } from "./handler";
 import { readHandler } from "./readHandler";
 
@@ -10,10 +10,23 @@ const expressApp: Express = express();
 //    resp.statusCode = 404;
 //    resp.end();
 //  })
+
 //  expressApp.get("*", basicHandler);
 expressApp.post("/read", readHandler);
-// Use the Express App static middleware with the .use method to route requests to serve static files from the static folder
+// Use Express file response enhancements
+expressApp.get("/sendcity", (req: Request, resp: Response) => {
+  resp.sendFile("city.png", {root: "static"});
+});
+expressApp.get("/downloadcity", (req: Request, resp: Response) => {
+  resp.download("static/city.png");
+});
+expressApp.get("/json", (req: Request, resp: Response) => {
+  resp.json("{name: Bob}");
+})
+
+// Use the Express App static middleware with the .use method to route requests to serve static files
 expressApp.use(express.static("static"));
+expressApp.use(express.static("node_modules/bootstrap/dist"));
 const server = createServer(expressApp);
 server.listen(port,
   () => console.log(`HTTP Server listening on port: ${port}`));
